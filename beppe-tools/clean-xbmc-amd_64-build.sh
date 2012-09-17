a759c59d145753f40a2187f39c0989ebb3be9951
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2012-09-17 10:24:45 +0200

    projects/AMD_NVIDIA/linux: Added new project for AMD+NVIDIA.
    Set the correct processor type for AMD.
    	With this flag the following processors are possible:
    	Athlon x2 7x50, Phenom II x3/x4, Phenom II, Athlon II x2/x3/x4, Turion II.
    	FX-8xxx/6xxx/4xxx (Bulldozer).
    
    Updated linux kernel configuration for AMD processors. Enbled driver for my hardware.
    Modified Linux Kernel configuration: added CPU frequency scaling driver for AMD processors.
    projects/AMD_NVIDIA/linux : add Marvell SAS support to supported projects. Updated to linux-3.2.31.
    Remove xbmc-frodo from buil helpers. From now on it's only xbmc.
    [helpers] Updated the ION build helper file to the new OE version.
    projects/AMD_NVIDIA/linux: Updated to linux-3-6.2.
    projects/AMD_NVIDIA/options: Updated options.
    projects/AMD_NVIDIA/linux: update configuration to linux 3.9.5
    projects/AMD_NVIDIA/options: update to Generic version

--- beppe-tools/clean-xbmc-amd_64-build.sh
+++ beppe-tools/clean-xbmc-amd_64-build.sh
@@ -3,8 +3,7 @@
 echo "rimuove le istanze di xbmc-frodo-pvr dal corrente albero di build"
 echo "l'albero di build è hardcoded"
 
-
-BUILDDIR="build.OpenELEC_frodo-AMD_NVIDIA.x86_64-devel"
+BUILDDIR="build.OpenELEC-AMD_NVIDIA.x86_64-devel"
 
 if [ -d "$BUILDDIR" ] ; then
   XBMC_DIR=`ls $BUILDDIR | grep xbmc-frodo-`
@@ -12,13 +11,13 @@ if [ -d "$BUILDDIR" ] ; then
   if [ "$XBMC_DIR" != "" ]; then
       rm -rf $BUILDDIR/$XBMC_DIR;
   fi
-  rm -rf "$BUILDDIR/.stamps/xbmc-frodo"
+  rm -rf "$BUILDDIR/.stamps/xbmc"
   XBMC_DIR=`ls $BUILDDIR | grep xbmc-frodo-theme-Confluence-`
   echo "--->$XBMC_DIR"
   if [ "$XBMC_DIR" != "" ]; then
       rm -rf $BUILDDIR/$XBMC_DIR;
   fi
-  rm -rf "$BUILDDIR/.stamps/xbmc-frodo-theme-Confluence"
+  rm -rf "$BUILDDIR/.stamps/xbmc-theme-Confluence"
 else
   echo "l'albero di build non esiste"
 fi
