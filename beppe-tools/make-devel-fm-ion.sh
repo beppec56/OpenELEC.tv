fd4f467b177fa00b95ae710ca41670106dca781e
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2013-08-31 08:29:19 +0200

    [helper files] Reworked build generic/amd+nvidia 64 bit helper. Fixed ION build helper

--- beppe-tools/make-devel-fm-ion.sh
+++ beppe-tools/make-devel-fm-ion.sh
@@ -7,11 +7,11 @@ DATE=`date +"%Y-%m-%d_%H%M%S"`
 
 ARCHIT=x86_64
 PRJ=ION
+XBMC_T=master
 
 GIT_BRANCH=`git status | grep "On branch" | awk '{print $4}'`
 GIT_SHASH=`git log -n1 --format=%h`
 LOG_FILE="../make-oe_fm_dev-"$PRJ"_"$ARCHIT"-"$GIT_BRANCH"_"$GIT_SHASH"-"$DATE".log"
-
 
 echo "Log file è: $LOG_FILE"
 echo "per leggere il log:"
@@ -19,6 +19,6 @@ echo "less $LOG_FILE"
 
 echo "`date +"%Y-%m-%d_%H%M%S"`" > "$LOG_FILE"
 
-PROJECT=$PRJ ARCH=$ARCHIT make release >> "$LOG_FILE" 2>&1
+PROJECT=$PRJ ARCH=$ARCHIT XBMC=$XBMC_T make release >> "$LOG_FILE" 2>&1
 
 echo "`date +"%Y-%m-%d_%H%M%S"`" >> "$LOG_FILE"
