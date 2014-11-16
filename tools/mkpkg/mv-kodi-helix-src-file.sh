d596b77a2263d787fa23218a558928ae979dc90f
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2014-11-16 18:13:19 +0100

    [helpers] Added helper file to move kodi sources into the correct place

---
+++ tools/mkpkg/mv-kodi-helix-src-file.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#$1 git checksum
+mv kodi-theme-Confluence-14-$1.tar.xz /var/www/openelec
+mv kodi-14-$1.tar.xz /var/www/openelec
+
+#mv xbmc-theme-Confluence-$1.tar.xz /var/www/openelec
+#mv xbmc-$1.tar.xz /var/www/openelec
