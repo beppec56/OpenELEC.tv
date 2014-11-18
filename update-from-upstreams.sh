bd6fa1c86abd1b504aa7cf70c2cb350a74914deb
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2014-11-18 10:17:11 +0100

    [helper files] Corrected to openelec-5.0 the helper bash script to update the repository from upstream

--- update-from-upstreams.sh
+++ update-from-upstreams.sh
@@ -5,7 +5,7 @@ echo "Current branch is: $CURRENT_BRANCH"
 git checkout master && git fetch upstream-ro && git pull upstream-ro +master:master
 #git fetch upstream-ro-xbmc
 #git checkout master-xbmc && git fetch upstream-ro-xbmc && git pull upstream-ro-xbmc +master:master-xbmc
-#attualizza openelec-4.0
-git checkout openelec-4.0
-git merge upstream-ro/openelec-4.0
+#attualizza openelec-5.0
+git checkout openelec-5.0
+git merge upstream-ro/openelec-5.0
 git checkout $CURRENT_BRANCH
